ff7af39a4d6ef9b7a08fe505d3d22cdcbeab0899
vadrums <vadlambrianov@gmail.com>
2025-01-30 18:59:08 +0300

    bla bla bla

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, "src", "index.js"),
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'index.[contenthash].js',
+    path: path.join(__dirname, "dist"),
+    filename: "index.[contenthash].js",
   },
   module: {
     rules: [
-        {
-            test: /\.js$/,
-            use: 'babel-loader',
-            exclude: /node_modules/,
-        },
-        {
-            test: /\.pug$/,
-            loader: 'pug-loader',
-        },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'template.pug'),
-      filename: 'index.html',
+      template: path.join(__dirname, "src", "template.pug"),
+      filename: "index.html",
     }),
     new FileManagerPlugin({
-        events: {
-            onStart: {
-                delete: ['dist'],
-            },
+      events: {
+        onStart: {
+          delete: ["dist"],
         },
+      },
     }),
   ],
   stats: {
-    children: true
-},
+    children: true,
+  },
   devServer: {
-    watchFiles: path.join(__dirname, 'src'),
+    watchFiles: path.join(__dirname, "src"),
     port: 9000,
   },
 };
